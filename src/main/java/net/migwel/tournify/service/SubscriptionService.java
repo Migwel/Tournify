7198adb4a72042ff801ec7a3963f55646e8343f1
miguel <migonzalez1991@gmail.com>
2018-07-23 21:19:18 +0200

    Remove useless null check

--- src/main/java/net/migwel/tournify/service/SubscriptionService.java
+++ src/main/java/net/migwel/tournify/service/SubscriptionService.java
@@ -15,16 +15,13 @@ public class SubscriptionService {
     @Autowired
     private ServiceFactory serviceFactory;
 
-    public Subscription addSubscription(String tournamentUrl, String callbackUrl) throws Exception {
+    public Subscription addSubscription(String tournamentUrl, String callbackUrl) {
         Subscription subscription = subscriptionRepository.findByTournamentUrlAndCallbackUrl(tournamentUrl, callbackUrl);
         if(subscription != null) {
             return  subscription;
         }
 
         TournamentService tournamentService = serviceFactory.getTournamentService(tournamentUrl);
-        if(tournamentService == null) {
-            throw new Exception("Couldn't find any service for url: "+ tournamentUrl); //TODO: Send more specific exception (and send it inside serviceFactory?)
-        }
         Tournament tournament = tournamentService.getTournament(tournamentUrl);
 
         subscription = new Subscription(tournament, callbackUrl);
