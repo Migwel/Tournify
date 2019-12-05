00781d6aaa10868266e67d0b95f964d5c3f4191e
miguel <migonzalez1991@gmail.com>
2019-12-05 22:43:21 +0100

    Fix Challonge test

--- challonge/src/test/java/dev/migwel/tournify/challonge/client/ChallongeClientTest.java
+++ challonge/src/test/java/dev/migwel/tournify/challonge/client/ChallongeClientTest.java
@@ -5,6 +5,7 @@ import dev.migwel.tournify.challonge.config.ChallongeConfiguration;
 import dev.migwel.tournify.challonge.impl.ChallongeUrlService;
 import dev.migwel.tournify.communication.commons.Player;
 import dev.migwel.tournify.core.client.TournamentClient;
+import dev.migwel.tournify.core.data.Tournament;
 import dev.migwel.tournify.core.exception.FetchException;
 import dev.migwel.tournify.core.http.HttpClient;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,7 +22,7 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.anyCollection;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.contains;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -33,7 +34,9 @@ public class ChallongeClientTest {
     private HttpClient httpClient =  mock(HttpClient.class);
     private ObjectMapper objectMapper = new ObjectMapper();
     private ChallongeFetcher challongeFetcher = new ChallongeFetcher(challongeConfiguration, challongeUrlService, httpClient, objectMapper);
-    private TournamentClient tournamentClient = new ChallongeClient(challongeFetcher, challongeUrlService);
+    private ChallongeTournamentFetcher challongeTournamentFetcher = new ChallongeTournamentFetcher(challongeUrlService, challongeFetcher);
+    private ChallongeMatchesFetcher challongeMatchesFetcher = new ChallongeMatchesFetcher(challongeUrlService, challongeFetcher);
+    private TournamentClient tournamentClient = new ChallongeClient(challongeFetcher, challongeUrlService, challongeTournamentFetcher, challongeMatchesFetcher);
 
     @BeforeEach
     void before() throws URISyntaxException, IOException {
@@ -41,11 +44,21 @@ public class ChallongeClientTest {
         challongeConfiguration.setApiToken("password");
         challongeConfiguration.setRetryNumber(3);
 
-        Path path = Paths.get(this.getClass().getClassLoader().getResource("ParticipantResponse.json").toURI());
+        String participantsJson = loadJson("ParticipantResponse.json");
+        String tournamentJson = loadJson("TournamentResponse.json");
+        String matchesJson = loadJson("MatchesResponse.json");
+        when(httpClient.get(contains("participants"), anyCollection())).thenReturn(participantsJson);
+        when(httpClient.get(contains("xgt2019nov.json"), anyCollection())).thenReturn(tournamentJson);
+        when(httpClient.get(contains("matches.json"), anyCollection())).thenReturn(matchesJson);;
+        ;
+    }
+
+    private String loadJson(String filename) throws URISyntaxException, IOException {
+        Path path = Paths.get(this.getClass().getClassLoader().getResource(filename).toURI());
         Stream<String> lines = Files.lines(path);
         String json = lines.collect(Collectors.joining("\n"));
         lines.close();
-        when(httpClient.get(anyString(), anyCollection())).thenReturn(json);
+        return json;
     }
 
     @Test
@@ -53,6 +66,12 @@ public class ChallongeClientTest {
         String url = "https://api.challonge.com/v1/tournaments/xgt2019nov";
         Set<Player> players = tournamentClient.getParticipants(url);
         assertTrue(players.contains(new Player("Adrien")));
+    }
 
+    @Test
+    void testGetTournament() throws FetchException {
+        String url = "https://api.challonge.com/v1/tournaments/xgt2019nov";
+        Tournament tournament = tournamentClient.fetchTournament(url);
+        System.out.println(tournament);
     }
 }
