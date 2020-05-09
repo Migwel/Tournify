179da5abe05d37d850663dd802fa760706efca9a
migwel <migonzalez1991@gmail.com>
2020-05-09 19:57:25 +0200

    Wrong table name

--- db/constraints/Set_constraints.sql
+++ db/constraints/Set_constraints.sql
@@ -3,5 +3,5 @@ ALTER TABLE public.set OWNER TO user_tournify;
 ALTER TABLE ONLY public.set
     ADD CONSTRAINT set_pkey PRIMARY KEY (id);
 
-ALTER TABLE ONLY public.tournament_phases
+ALTER TABLE ONLY public.set
     ADD CONSTRAINT fkp7433nons6n9n9kg2e9ciggqp FOREIGN KEY (phase_id) REFERENCES public.phase(id);
